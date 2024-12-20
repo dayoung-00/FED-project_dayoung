6e3a8022542e1cec16b7e274d7d0d17f03ab6284
Tom B. Parker <tombap8@hotmail.com>
2024-12-20 16:35:10 +0900

    ss

--- 1차_프로젝트/02.구현소스/js/main.js
+++ 1차_프로젝트/02.구현소스/js/main.js
@@ -7,7 +7,7 @@ import slideFn from "./slide_fn.js";
 // 슬라이드함수 호출하여 실행하기
 slideFn();
 
-var swiper = new Swiper(".model-box", {
+const swiper = new Swiper(".model-box", {
     slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
@@ -15,4 +15,28 @@ var swiper = new Swiper(".model-box", {
       clickable: true,
     },
   });
+const swiper2 = new Swiper("#news-box", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // 중앙으로 이미지 오도록함
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // 배경이미지 이름 만들기
+const bgImageArr = [];
+for(let i=1;i<=9;i++) bgImageArr[i] = 'awards_0'+i;
+console.log(bgImageArr);
+
+    const bgImage = document.querySelector("#news-box>img");
+
+  // 이벤트 발생시 배경 이미지 변경하기
+  swiper2.on('slideChange',()=>{
+    const currIdx = swiper2.activeIndex;
+    console.log(currIdx);
+    bgImage.src = `./image/${bgImageArr[currIdx+1]}.jpg`;
+  })
 
